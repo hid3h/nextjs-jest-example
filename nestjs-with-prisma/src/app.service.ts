5eda6ac495b9a3ae2ac50684bb7a62a0e0e5c6f9
hid3h <razyetts@hotmail.co.jp>
2023-12-26 09:48:20 +0900

    prisma, service

--- nestjs-with-prisma/src/app.service.ts
+++ nestjs-with-prisma/src/app.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+  constructor(private prisma: PrismaService) {}
+
+  issueInvoice = async ({ amount }: { amount: number }) => {
+    const invoice = await this.prisma.invoiceIssuance.create({
+      data: {
+        amount,
+        issuedAt: new Date(),
+      },
+    });
+    return invoice;
+  };
 }
