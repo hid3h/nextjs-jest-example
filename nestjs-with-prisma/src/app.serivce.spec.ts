bec6dd4e64e51be55d847e747ca7b8a5fcf94b3b
hid3h <razyetts@hotmail.co.jp>
2023-12-26 09:59:07 +0900

    ちょっと変更

--- nestjs-with-prisma/src/app.serivce.spec.ts
+++ nestjs-with-prisma/src/app.serivce.spec.ts
@@ -2,20 +2,29 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-describe('AppController', () => {
-  let app: TestingModule;
+describe('請求書', () => {
+  let appSerivce: AppService;
 
   beforeAll(async () => {
-    app = await Test.createTestingModule({
+    const moduleRef = await Test.createTestingModule({
       controllers: [AppController],
       providers: [AppService],
     }).compile();
+
+    appSerivce = moduleRef.get<AppService>(AppService);
   });
 
-  describe('getHello', () => {
-    it('should return "Hello World!"', () => {
-      const appController = app.get(AppController);
-      expect(appController.getHello()).toBe('Hello World!');
-    });
+  it('請求書発行', async () => {
+    await appSerivce.issueInvoice({ amount: 1000 });
+
+    const invoiceIssuance = await prisma.invoiceIssuance.findFirst();
+    expect(invoiceIssuance?.amount).toBe(1000);
   });
+
+  // describe('getHello', () => {
+  //   it('should return "Hello World!"', () => {
+  //     const appController = app.get(AppController);
+  //     expect(appController.getHello()).toBe('Hello World!');
+  //   });
+  // });
 });
