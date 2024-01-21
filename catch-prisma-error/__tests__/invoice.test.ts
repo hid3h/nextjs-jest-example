5c002ae13e3557750eeffc95017e7cb8f38c6960
hid3h <razyetts@hotmail.co.jp>
2024-01-21 10:37:49 +0900

    prismaのエラーをキャッチできない

--- catch-prisma-error/__tests__/invoice.test.ts
+++ catch-prisma-error/__tests__/invoice.test.ts
@@ -1,7 +1,4 @@
-import {
-  fetchLatestInvoice,
-  issueInvoice,
-} from "../app/services/invoice-serivce";
+import { issueInvoice } from "../app/services/invoice-serivce";
 import prisma from "../db";
 
 test("請求書発行", async () => {
@@ -10,11 +7,3 @@ test("請求書発行", async () => {
   const invoiceIssuance = await prisma.invoiceIssuance.findFirst();
   expect(invoiceIssuance?.amount).toBe(1000);
 });
-
-// test("最新の請求書取得", async () => {
-//   await issueInvoice({ amount: 1000 });
-//   await issueInvoice({ amount: 2000 });
-
-//   const latestInvoiceIssuance = await fetchLatestInvoice();
-//   expect(latestInvoiceIssuance?.amount).toBe(2000);
-// });
--- catch-prisma-error/app/services/invoice-serivce.ts
+++ catch-prisma-error/app/services/invoice-serivce.ts
@@ -20,7 +20,6 @@ export const issueInvoice = async ({ amount }: { amount: number }) => {
     // });
     const invoice = await prisma.invoiceIssuance.create({
       data: {
-        id: "1",
         amount,
         issuedAt: new Date(),
       },
@@ -28,39 +27,18 @@ export const issueInvoice = async ({ amount }: { amount: number }) => {
 
     await prisma.invoiceIssuance.create({
       data: {
-        id: "1",
+        id: invoice.id,
         amount,
         issuedAt: new Date(),
       },
     });
   } catch (e) {
-    console.log(e);
-    console.log(
-      "e instanceof Prisma.PrismaClientKnownRequestError",
-      PrismaClientKnownRequestError
-    );
-    console.log(
-      "PrismaClientKnownRequestError.prototype",
-      PrismaClientKnownRequestError.prototype
-    );
-
-    if (e instanceof HogeError) {
-      console.log("HogeError", e.fuga);
-    }
-
+    console.log("e", e);
     if (e instanceof PrismaClientKnownRequestError) {
-      console.log("入ってますか？", e.code);
+      console.log("入りました", e.code);
       console.log(e.meta);
+    } else {
+      console.log("入っていません");
     }
   }
-  // return invoice;
-};
-
-export const fetchLatestInvoice = async () => {
-  const invoice = await prisma.invoiceIssuance.findFirst({
-    orderBy: {
-      issuedAt: "desc",
-    },
-  });
-  return invoice;
 };
